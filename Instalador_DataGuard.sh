cb620654ebfe160c060814603dff42ac4a3b0235
InacioFigueiredo13 <inaciofigueiredo13@gmail.com>
2023-10-17 19:59:36 -0300

    Upload do script de instalação do DataGuard

---
+++ Instalador_DataGuard.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+title="INSTALAÇÃO - DataGuard"
+prompt="Você gostaria de instalar o DataGuard?"
+options=("Sim" "Não")
+echo "$title"
+
+PS3="$prompt "
+
+updateAndUpgrade() {
+    clear
+    echo "Atualizando pacotes para iniciar a instalação..."
+    sudo apt update && sudo apt upgrade -y
+}
+
+verificarJava() {
+    clear
+    echo "Pacotes atualizados!"
+    echo "Verificando a versão do Java..."
+    java -version
+    if [ $? = 0 ]; then
+        echo “java instalado na versão $?”
+    else
+        echo "java não instalado"
+        echo "Gostaria de instalar o Java? [s/n]"
+        read get
+        if [ \“$get\” == \“s\” ]; then
+            echo "Instalando o Java 17..."
+            sudo apt install openjdk-17-jre -y
+        else
+            echo "O Java não foi instalado, não foi possível completar a instalação do DataGuard."
+            exit 1
+        fi
+    fi
+}
+
+instalarJar() {
+    clear
+    ls | grep "Looca.jar"
+
+    if [ $? = 0 ]; then
+        echo "DataGuard já instalado"
+        echo "Iniciando o DataGuard."
+        java -jar Looca.jar
+    else
+        echo "Instalando o DataGuard..."
+        curl -o Looca.jar -LJO https://github.com/EigTech-Solutions/JAR---DataGuard/raw/main/Looca.jar #trocar para o nosso prj
+        if [ $? -eq 0 ]; then
+            echo "Instalação concluída!"
+            echo "Iniciando o DataGuard."
+            java -jar Looca.jar
+        else
+            echo "O curl encontrou um erro."
+        fi
+    fi
+}
+
+menu() {
+    select item in "${options[@]}"; do
+        case $REPLY in
+        1)
+            echo "Iniciando a instalação..."
+            updateAndUpgrade
+            verificarJava
+            instalarJar
+            break
+            ;;
+        2)
+            echo "Instalação cancelada!"
+            break
+            ;;
+        *) echo "Por favor, escolha uma das opções acima" ;;
+        esac
+    done
+}
+
+menu
